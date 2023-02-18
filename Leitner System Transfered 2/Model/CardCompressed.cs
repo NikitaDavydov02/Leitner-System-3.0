42cf013147c677702e73c5e9a93c05590fcd92dd
Nikita Davydov <nikita.davidov02@yandex.ru>
2023-02-18 16:24:56 +0300

    developStable

---
+++ Leitner System Transfered 2/Model/CardCompressed.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace Leitner_System_Transfered_2.Model
+{
+    [DataContract]
+    class CardCompressed
+    {
+        //--------------------------------------------------------------------------------
+        //------------------------------------- DATA MEMBERS ---------------------------------
+        //--------------------------------------------------------------------------------
+        [DataMember]
+        public string Question { get;set; }
+        [DataMember]
+        public string Answer { get; set; }
+        [DataMember]
+        public byte[] QuestionImageByte { get; set; }
+        [DataMember]
+        public byte[] AnswerImageByte { get; set; }
+        [DataMember]
+        public DateTime LastRepetitionTime { get; private set; }
+        [DataMember]
+        public RepitionFrequensy RepitionFrequensy { get; private set; }
+        [DataMember]
+        public DateTime LastReverseRepetitionTime { get; private set; }
+        [DataMember]
+        public RepitionFrequensy ReverseRepitionFrequensy { get; private set; }
+        public Deck ParentDeck { get; set; }
+        //--------------------------------------------------------------------------------
+        //------------------------------------- METHODS ---------------------------------
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Create new Card, attached to the Deck
+        /// </summary>
+        /// <param name="parentDeck">Deck that is parent to this card</param>
+        /// <param name="question">Question of this card</param>
+        /// <param name="answer">Answer of this card</param>
+        /// <param name="relativeToDeckFolderAnswerImagePath">Relative to deck folder path of answer image</param>
+        /// <param name="relativeToDeckFolderQuestionImagePath">Relative to deck folder path of question image</param>
+        //public CardCompressed(Deck parentDeck, string question, string answer, string relativeToFoderWithDecksAnswerImagePath = "", string relativeToFoderWithDecksQuestionImagePath = "")
+        //{
+        //    this.ParentDeck = parentDeck;
+        //    Question = question;
+        //    Answer = answer;
+        //    RelativeToDeckFolderQuestionImagePath = relativeToFoderWithDecksQuestionImagePath;
+        //    RelativeToDeckFolderAnswerImagePath = relativeToFoderWithDecksAnswerImagePath;
+        //    LastRepetitionTime = DateTime.Now;
+        //    LastReverseRepetitionTime = DateTime.Now;
+        //    RepitionFrequensy = RepitionFrequensy.Daily;
+        //    ReverseRepitionFrequensy = RepitionFrequensy.Daily;
+        //}
+    }
+}
---
+++ Leitner System Transfered 2/Model/DeckCompressed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+using System.IO;
+namespace Leitner_System_Transfered_2.Model
+{
+    [DataContract]
+    class DeckCompressed
+    {
+        //--------------------------------------------------------------------------------
+        //------------------------------------- DATA MEMBERS ---------------------------------
+        //--------------------------------------------------------------------------------
+        [DataMember]
+        public List<Card> Cards { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
